4cea2d0e71cbaaa0ecd4e5cc9db925993face324
bleached123 <joshuapboothroyd@gmail.com>
2018-01-24 11:57:27 +0000

    Updated without <center>

--- BillySite/updating.php
+++ BillySite/updating.php
@@ -23,11 +23,9 @@
     </head>
     <body>
         <?php include 'includes/header.html'?>
-        <center>
             <div class="row" style="background-color: #80B8EA;">
                 <h1 style=" font-size: 70px; color:white;"><img src="img/Billy%20Happy%20small.png">Updating<img src="img/Billy%20Happy%20small.png" style="transform:scaleX(-1)"></h1>
             </div>
-        </center>
             <?php include 'includes/footer.html'?>
     </body>
 </html>
